450c4e61ec405b0ffc1c861bd8a32885fbd503e1
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-29 20:44:52 -0500

    Update juego_adivinanza.py

--- juego_adivinanza.py
+++ juego_adivinanza.py
@@ -9,16 +9,3 @@ def juego_adivinanza():
     while intentos < max_intentos:
         intento = int(input("Introduce tu suposición: "))
 
-        if intento < numero_secreto:
-            print("El número secreto es mayor. Intenta de nuevo.")
-        elif intento > numero_secreto:
-            print("El número secreto es menor. Intenta de nuevo.")
-        else:
-            print(f"¡Felicidades! ¡Adivinaste el número secreto ({numero_secreto}) en {intentos + 1} intentos!")
-            break
-
-        intentos += 1
-    if intentos == max_intentos:
-        print(f"¡Agotaste tus {max_intentos} intentos! El número secreto era {numero_secreto}. ¡Mejor suerte la próxima vez!")
-if __name__ == "__main__":
-    juego_adivinanza()
