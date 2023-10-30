98a5b6143d015508ff18a10ccee83e9cf5912091
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-29 20:45:08 -0500

    Update juego_adivinanza.py

--- juego_adivinanza.py
+++ juego_adivinanza.py
@@ -1,13 +1,7 @@
 import random
 def juego_adivinanza():
     """Este funcion lo que hará es crear un juego de adivinanza de verdadero y falso"""
-    numero_secreto = random.randint(1, 100)
-    intentos = 0
-    max_intentos = 10
-    print("¡Bienvenido al juego de adivinanza!")
-    print(f"Adivina el número entre 1 y 100. Tienes un máximo de {max_intentos} intentos.")
-    while intentos < max_intentos:
-        intento = int(input("Introduce tu suposición: "))
+ 
 
         if intento < numero_secreto:
             print("El número secreto es mayor. Intenta de nuevo.")
