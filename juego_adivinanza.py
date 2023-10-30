08a0d22aa8459d8179fd77405da766d64944805b
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-29 20:39:29 -0500

    Update juego_adivinanza.py

--- juego_adivinanza.py
+++ juego_adivinanza.py
@@ -1,3 +1,6 @@
 import random
 def juego_adivinanza():
     """Este funcion lo que hará es crear un juego de adivinanza de verdadero y falso"""
+    numero_secreto = random.randint(1, 100)
+    intentos = 0
+    max_intentos = 10
